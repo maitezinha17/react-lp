4b971ac09607873011fd1edda4674da43275c4a5
Maite <maitefranca08@gmail.com>
2026-06-08 08:18:46 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import s from './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <nav>
+        <span className={s.navItems} onclick="navTo('s1')" >
+          <img src="../assets/icons/BotaoProdutos.png" alt="Jogo" width="30px" />
+          <a className={s.navWords} >Produtos</a>
+        </span>
+        <span className={s.navItems} onclick="navTo('s2')">
+          <img src="../assets/icons/BotaoSobre.png" alt="Carros" width="30px" />
+          <a className={s.navWords} >Sobre</a>
+        </span>
+        <span className={s.navItems} onclick="navTo('s3')">
+          <img src="../assets/icons/BotaoVideo.png" alt="Vídeo" width="30px" />
+          <a className={s.navWords} >Vídeo</a>
+        </span>
+        <span className={s.navItems} onclick="navTo('s4')">
+          <img src="../assets/icons/BotaoContato.png" alt="Contato" width="30px" />
+          <a className={s.navWords} >Contato</a>
+        </span>
+      </nav>
 
-      <div className="ticks"></div>
+      <main>
+        <section id="s1" className={s.s1}>
+          <h1>Bebes  </h1>
+          <h2>Eu amo bebes</h2>
+        </section>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <section id="s2">
+          <div className={s.s2}>
+            <img width="300px" height="auto" src="../assets/imagens/Bebe1.jpg" alt="Bebe 1" />
+            <div>
+              <h4>Bebe 1</h4>
+              <p>Descrição do bebe 1</p>
+            </div>
+          </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          <div className={s.card}>
+            <img width="300px" height="auto" src="../assets/imagens/26d3b3e4bffd959f4cd9490be60dc666.jpg" alt="Bebe 2" />
+            <div>
+              <h4>Bebe 2</h4>
+              <p>Descrição do bebe 2</p>
+            </div>
+          </div>
+
+          <div className={s.card}>
+            <img width="300px" height="auto" src="../assets/imagens/169232da292f714cbb74fb1a6fdc5eee.jpg" alt="Bebe 3" />
+            <div>
+              <h4>Bebe 3</h4>
+              <p>Descrição do bebe 3</p>
+            </div>
+          </div>
+        </section>
+
+        <section id={s.s3}>
+          <iframe width="560" height="315" className="video" src="https://www.youtube.com/embed/_MtwmmPnEHA?si=igJsk8OtIj5rxPDb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        </section>
+
+      </main>
+      <footer>
+        <a href="https://www.facebook.com" target="_blank">
+          <img width="40px" src="../assets/icons/facebook.png" alt="facebook" />
+        </a>
+        <a href="https://www.youtube.com" target="_blank">
+          <img width="40px" src="../assets/icons/youtube.png" alt="youtube" />
+        </a>
+        <a href="https://www.whatsapp.com" target="_blank">
+          <img width="40px" src="../assets/icons/whatsapp.png" alt="whatsapp" />
+        </a>
+      </footer>
     </>
   )
 }
